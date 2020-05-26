5feb314a3b24c73b5103ffa6b99dd3633d9c74d2
jun6231jp <a.miki.21.29.0232@gmail.com>
2020-05-27 01:07:28 +0900

    Update logic.cpp

--- logic.cpp
+++ logic.cpp
@@ -19,42 +19,48 @@ using namespace std;
  * 8. インライン関数
  * 9. C版以上に高速
  * */
+
 typedef vector< int > vec;
 typedef vector< vec > vec2;
 typedef vector< vec2 > vec3;
 
 class LookUpTable{
 private:
-   int ptnlen;
-   int linenum;
+   int PatternLength;
+   int LineNum;
+   int OptLineNum;
 public:
    LookUpTable();
    ~LookUpTable();
-   int fileread(char* filename);
-   vector<string> pattable;
-   vector<string> value;
-   vector<int> pattern;
-   vector<int> truth;
+   int FileRead(char* filename);
+   void TableOpt();
+   vector<string> PatternTable;
+   vector<string> ValueList;
+   vec PatternNumList;
+   vec TruthNumList;
+   vec TableMask;
+   vec OptPatternNumList;
+   vec OptTruthNumList;
 };
-class table{
+class Table{
 private:
-   int linenum;
-   int width;
+   int LineNum;
+   int Width;
 public:
-   vec2 bittable;
-   table(int bitwidth);
-   ~table();
+   vec2 BitTable;
+   Table(int bitwidth);
+   ~Table();
    int add();
    void write(int linenum, int ad , int val);
    int read(int linenum, int ad);
 };
-class list{
+class List{
 private:
-  int tablenum;
+  int TableNum;
 public:
-  vector<table> t_list;
-  list();
-  ~list();
+  vector<Table> TableList;
+  List();
+  ~List();
   void add();
   void del();
 };
@@ -62,177 +68,185 @@ public:
 LookUpTable::LookUpTable()
 {
  cout << "LUT construct" << endl;
- linenum=0;
- ptnlen=0;
+ LineNum=0;
+ PatternLength=0;
 }
 LookUpTable::~LookUpTable()
 {
  cout << "LUT destruct" << endl;
 }
-
-int LookUpTable::fileread(char* filename)
+int LookUpTable::FileRead(char* filename)
 {
   ifstream ifs(filename);
   string str;
   if (ifs.fail())
-  {
     return -1;
-  }
   while (getline(ifs, str))
   {
     while (str[0]==' ')//先頭のスペース削除
       str = str.substr(1);
     if (str[0]=='\0') //空行無視
       continue;
-    if (ptnlen!=0 && ptnlen!=str.find(' '))//パターン長が一定でなければエラー
+    if (PatternLength!=0 && PatternLength!=str.find(' '))//パターン長が一定でなければエラー
       return -1;
-    linenum++;
-    ptnlen=str.find(' ');
-    pattable.resize(linenum);
-    value.resize(linenum);
-    pattern.resize(linenum);
-    truth.resize(linenum);
-    pattable[linenum-1] = str.substr(0, ptnlen);
-    value[linenum-1] = str.substr(ptnlen + 1);
-    while (value[linenum-1][0]==' ')//パターンと真理値の間の空白が複数あれば切り詰める
-        value[linenum-1] = value[linenum-1].substr(1);
-    if (value[linenum-1][0]!='0' && value[linenum-1][0]!='1')//真理値無しであればエラー
+    LineNum++;
+    PatternLength=str.find(' ');
+    PatternTable.resize(LineNum);
+    ValueList.resize(LineNum);
+    PatternNumList.resize(LineNum);
+    TruthNumList.resize(LineNum);
+    PatternTable[LineNum-1] = str.substr(0, PatternLength);
+    ValueList[LineNum-1] = str.substr(PatternLength + 1);
+    while (ValueList[LineNum-1][0]==' ')//パターンと真理値の間の空白が複数あれば切り詰める
+        ValueList[LineNum-1] = ValueList[LineNum-1].substr(1);
+    if (ValueList[LineNum-1][0]!='0' && ValueList[LineNum-1][0]!='1')//真理値無しであればエラー
        return -1;
-    if (value[linenum-1][1]!='\0' &&  value[linenum-1][1]!=' ')//真理値が2文字以上ならエラー
+    if (ValueList[LineNum-1][1]!='\0' && ValueList[LineNum-1][1]!=' ')//真理値が2文字以上ならエラー
        return -1;
-    value[linenum-1] = value[linenum-1].substr(0,1);
+    ValueList[LineNum-1] = ValueList[LineNum-1].substr(0,1);
     //パターンを数値変換
-    for (int i = ptnlen-1 ; i >=0 ; i--)
+    for (int i = 0 ; i < PatternLength ; i++)
     {
-      if (pattable[linenum-1][i]=='1')
-      {
-        pattern[linenum-1]+=pow(2,ptnlen-1-i);
-      }
-      else if (pattable[linenum-1][i]!='0')
+      if (PatternTable[LineNum-1][i]=='1')
+        PatternNumList[LineNum-1]+=pow(2,i);
+      else if (PatternTable[LineNum-1][i]!='0')
        return -1; //0, 1以外の文字が使われていればエラー
     }
     //真理値を数値変換
-    istringstream(value[linenum-1]) >> truth[linenum-1];
-    cout << pattable[linenum-1] << "/" << pattern[linenum-1] << ":" << value[linenum-1] << "/" << truth[linenum-1] << endl;
+    istringstream(ValueList[LineNum-1]) >> TruthNumList[LineNum-1];
+    cout << PatternNumList[LineNum-1] << ":" << TruthNumList[LineNum-1] << endl;
   }
   //パターン重複があればエラー
-  sort(pattable.begin(), pattable.end());
-  if (unique(pattable.begin(), pattable.end()) < pattable.end())
+  sort(PatternTable.begin(), PatternTable.end());
+  if (unique(PatternTable.begin(), PatternTable.end()) < PatternTable.end())
     return -1;
-  cout <<  "pattern length : " << ptnlen << endl;
-  cout << "table lines : " << linenum << endl;
+  cout <<  "pattern length : " << PatternLength << endl;
+  cout << "table lines : " << LineNum << endl;
   return 0;
 }
-
-list::list()
+void LookUpTable::TableOpt()
 {
- cout << "list construct" << endl;
- tablenum=0;
+  vec NewPatternNumList;
+  vec NewTruthNumList;
+  vec MaskBuff;
+  vec MaskBitNumList;
+  NewPatternNumList.resize(LineNum);
+  NewTruthNumList.resize(LineNum);
+  MaskBuff.resize(PatternLength); 
+  MaskBitNumList.resize(PatternLength);
+ for(int m=0 ; m < PatternLength; m++)
+ {
+  for(int i = 0; i < LineNum; i++)
+  {
+    NewPatternNumList[i]=PatternNumList[i];
+    NewTruthNumList[i]=TruthNumList[i];
+  }
+  for(int i = m; i < PatternLength + m; i++)
+  {
+    int id = i;
+    if (id >= PatternLength)
+      id -= PatternLength;
+    MaskBuff[id]=1;
+    for(int j = 0; j < LineNum; j++)
+    { 
+      NewPatternNumList[j]=NewPatternNumList[j]%(int)pow(2,id) + NewPatternNumList[j]/(int)pow(2,(id+1));
+      for(int k = 0; k < j; k++)
+      {
+        if(NewPatternNumList[j]==NewPatternNumList[k] && NewTruthNumList[j]==NewTruthNumList[k])
+        {
+          NewTruthNumList[j]=2;
+          break;
+        }  
+        else if(NewPatternNumList[j]==NewPatternNumList[k] && NewTruthNumList[j]!=NewTruthNumList[k] && NewTruthNumList[j]!=2)          {
+          MaskBuff[id]=0;
+          for(int l=0; l <= j ; l++)
+          {
+            NewPatternNumList[j]=NewPatternNumList[j] + (int)pow(2,id);
+          }
+          break; 
+        }     
+      }
+      if(MaskBuff[id]==0)
+      {
+        break;
+      }
+    } 
+  }
+  for(int i=0; i < PatternLength; i++)
+  {
+    if(MaskBuff[i]==1)
+      MaskBitNumList[m]++;
+  }
+  cout << "mask:" << MaskBitNumList[m] << endl;  
+ }
 }
 
-list::~list()
+List::List()
+{
+ cout << "list construct" << endl;
+ TableNum=0;
+}
+
+List::~List()
 {
  cout << "list destruct" << endl;
 }
 
-void list::add()
+void List::add()
 {
- tablenum++;
- int width=pow(2,tablenum);
- t_list.resize(tablenum,table(width));
- cout << "table " << tablenum << "created" << endl;
+ TableNum++;
+ int width=pow(2,TableNum);
+ TableList.resize(TableNum,Table(width));
+ cout << "table " << TableNum << "created" << endl;
 }
 
-void list::del()
+void List::del()
 {
- cout << "table " << tablenum << "deleted" << endl;
- tablenum--;
- int width=pow(2,tablenum);
- t_list.resize(tablenum,table(width));
+ cout << "table " << TableNum << "deleted" << endl;
+ TableNum--;
+ int width=pow(2,TableNum);
+ TableList.resize(TableNum,Table(width));
 }
 
-table::table(int bitwidth)
+Table::Table(int bitwidth)
 {
-  linenum=0;
-  width=bitwidth;
-  vec2 bittable(1,vec(1,0));
+  LineNum=0;
+  Width=bitwidth;
+  vec2 BitTable(1,vec(1,0));
   cout << "table construct" << endl;
 }
-table::~table()
+Table::~Table()
 {
   cout << "table destruct" << endl;
 }
 
-int table::add()
+int Table::add()
 {
-  linenum++;
-  bittable.resize(linenum,vec(width,0));
-  cout << "line added width:" << width << endl;
-  return linenum;
+  LineNum++;
+  BitTable.resize(LineNum,vec(Width,0));
+  cout << "line added width:" << Width << endl;
+  return LineNum;
 }
 
-void table::write(int line,int ad , int val)
+void Table::write(int line, int ad, int val)
 {
-  bittable[line][ad]=val;
+  BitTable[line][ad]=val;
 }
 
-int table::read(int line,int ad)
+int Table::read(int line,int ad)
 {
-  return bittable[line][ad];
+  return BitTable[line][ad];
 }
 
 int main (int argc, char* argv[]){
     LookUpTable lut;
-    if (lut.fileread(argv[1])!=0)
+    if (lut.FileRead(argv[1])!=0)
     {
       cout << "table err" << endl;
       return -1;
     }
-    list l;
-    int readval[8]={0};
-    l.add();
-    l.t_list[0].add();
-    l.t_list[0].write(0,0,1);
-    readval[0]=l.t_list[0].read(0,0);
-    readval[1]=l.t_list[0].read(0,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    l.add();
-    readval[0]=l.t_list[0].read(0,0);
-    readval[1]=l.t_list[0].read(0,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    l.t_list[1].add();
-    l.t_list[1].write(0,0,1);
-    l.t_list[1].add();
-    l.t_list[1].write(1,1,2);
-    readval[0]=l.t_list[1].read(0,0);
-    readval[1]=l.t_list[1].read(0,1);
-    readval[2]=l.t_list[1].read(0,2);
-    readval[3]=l.t_list[1].read(0,3);
-    cout << readval[0] << " " <<  readval[1] << " " << readval[2] << " " <<  readval[3] << endl;
-    readval[0]=l.t_list[1].read(1,0);
-    readval[1]=l.t_list[1].read(1,1);
-    readval[2]=l.t_list[1].read(1,2);
-    readval[3]=l.t_list[1].read(1,3);
-    cout << readval[0] << " " <<  readval[1] << " " << readval[2] << " " <<  readval[3] << endl;
-    l.t_list[0].add();
-    l.t_list[0].write(1,1,3);
-    readval[0]=l.t_list[0].read(0,0);
-    readval[1]=l.t_list[0].read(0,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    readval[0]=l.t_list[0].read(1,0);
-    readval[1]=l.t_list[0].read(1,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    l.del();
-    l.t_list[0].add();
-    l.t_list[0].write(2,0,4);
-    readval[0]=l.t_list[0].read(0,0);
-    readval[1]=l.t_list[0].read(0,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    readval[0]=l.t_list[0].read(1,0);
-    readval[1]=l.t_list[0].read(1,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
-    readval[0]=l.t_list[0].read(2,0);
-    readval[1]=l.t_list[0].read(2,1);
-    cout <<  readval[0] << " " <<  readval[1] << endl;
+    lut.TableOpt();
+    List l;
+  
     return 0;
 }
